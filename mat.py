68f081c1d0d93bfc876cece8a95337627dcecc5f
LeoDevPy <56728396+LeoDevPy@users.noreply.github.com>
2020-05-29 02:05:36 -0300

    Gráfico
    Criação de Gráfico simples com a biblioteca matplotlib em python.

---
+++ mat.py
@@ -0,0 +1,17 @@
+import matplotlib as mpl 
+import matplotlib.pyplot as plt 
+
+
+# plt.plot([1,3,5],[2,5,7])
+# plt.show()
+
+
+x = [1,4,5]
+
+y = [3,7,2]
+
+plt.plot(x,y)
+plt.xlabel('Váriavel 1')
+plt.ylabel('Variavel 2')
+plt.title('testeplot')
+plt.show()
